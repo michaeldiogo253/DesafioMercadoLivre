48e4f48fb142b56b99d7a18f0d8efd70c00ab59e
michaeldiogo253 <64430518+michaeldiogo253@users.noreply.github.com>
2021-10-03 16:44:29 -0300

    Update Desafio.java

--- Desafio4Digitos/src/Desafio.java
+++ Desafio4Digitos/src/Desafio.java
@@ -1,76 +1,88 @@
+package vagas;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class Desafio {
 
-	public static void main(String[] args) {
 
-		int ValorDigitado = 6;
+class Result {
 
-		List<Integer> numeros = new ArrayList<>(Valor4Digitos(ValorDigitado));
+   
+    public static List<Integer> calculation(int maxDigit) {
 
-		for (Integer numero : numeros) {
-			System.out.println(numero);
-		}
+      List<Integer> digitos21 = new ArrayList<>();
 
-	}
+        String n1 = String.valueOf(maxDigit);
+        String n2 = String.valueOf(maxDigit);
+        String n3 = String.valueOf(maxDigit);
+        String n4 = String.valueOf(maxDigit);
+        String valorMaxString = n1 + n2 + n3 + n4;
+        int valMax = Integer.parseInt(valorMaxString);
 
-	public static List<Integer> Valor4Digitos(int valorDigitado) {
+        for (int i = 5555; i <= valMax; i++) {
 
-		// OperaçoesString op = new OperaçoesString();
-		List<Integer> digitos21 = new ArrayList<>();
+            String numero = String.valueOf(i);
+            char dig1 = numero.charAt(3);
+            char dig2 = numero.charAt(2);
+            char dig3 = numero.charAt(1);
+            char dig4 = numero.charAt(0);
 
-		if (valorDigitado <= 5 || valorDigitado > 9) {
-			return digitos21;
-		}
+            if (VerificaIgual21(dig1, dig2, dig3, dig4, maxDigit)) {
+                digitos21.add(Integer.parseInt(numero));
+            }
 
-		String n1 = String.valueOf(valorDigitado);
-		String n2 = String.valueOf(valorDigitado);
-		String n3 = String.valueOf(valorDigitado);
-		String n4 = String.valueOf(valorDigitado);
-		String valorMax = n1 + n2 + n3 + n4;
-		int ValMax = Integer.parseInt(valorMax);
+        }
 
-		for (int i = 5555; i <= ValMax; i++) {
+        return digitos21;
 
-			String numero = String.valueOf(i);
-			char dig1 = numero.charAt(3);
-			char dig2 = numero.charAt(2);
-			char dig3 = numero.charAt(1);
-			char dig4 = numero.charAt(0);
+    }
+    
+    public static boolean VerificaIgual21(char n1, char n2, char n3, char n4, int valorDigitado) {
+        String caracter1 = String.valueOf(n1);
+        int x1 = Integer.parseInt(caracter1);
+        String caracter2 = String.valueOf(n2);
+        int x2 = Integer.parseInt(caracter2);
+        String caracter3 = String.valueOf(n3);
+        int x3 = Integer.parseInt(caracter3);
+        String caracter4 = String.valueOf(n4);
+        int x4 = Integer.parseInt(caracter4);
 
-			if (VerificaIgual21(dig1, dig2, dig3, dig4, valorDigitado)) {
-				digitos21.add(Integer.parseInt(numero));
-			}
+        if (x1 > valorDigitado || x2 > valorDigitado || x3 > valorDigitado || x4 > valorDigitado) {
+            return false;
+        }
 
-		}
+        int soma = x1 + x2 + x3 + x4;
 
-		return digitos21;
+        if (soma == 21) {
+            return true;
+        } else {
+            return false;
+        }
 
-	}
+    }
+    
 
-	public static boolean VerificaIgual21(char n1, char n2, char n3, char n4, int ValorDigitado) {
-		String caracter1 = String.valueOf(n1);
-		int x1 = Integer.parseInt(caracter1);
-		String caracter2 = String.valueOf(n2);
-		int x2 = Integer.parseInt(caracter2);
-		String caracter3 = String.valueOf(n3);
-		int x3 = Integer.parseInt(caracter3);
-		String caracter4 = String.valueOf(n4);
-		int x4 = Integer.parseInt(caracter4);
+}
 
-		if (x1 > ValorDigitado || x2 > ValorDigitado || x3 > ValorDigitado || x4 > ValorDigitado) {
-			return false;
-		}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        
+        Scanner sc = new Scanner(System.in);
+        int valorDigitado;
+        System.out.println("Informe o valor maximo");
+        valorDigitado = sc.nextInt();
+    
+        List<Integer> numeros = Result.calculation(valorDigitado);
 
-		int soma = x1 + x2 + x3 + x4;
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+        }
 
-		if (soma == 21) {
-			return true;
-		} else {
-			return false;
-		}
-
-	}
-
+        sc.close(); 
+        
+        
+      
+    }
 }
